ea70ee087e93b881d2c2854da9a4219f8f2ed539
David Díaz González <dgonzalez@suse.de>
2022-10-11 17:15:24 +0100

    Adapt NetworkWiredStatus to allow editing connections

--- web/src/NetworkWiredStatus.jsx
+++ web/src/NetworkWiredStatus.jsx
@@ -19,21 +19,97 @@
  * find current contact information at www.suse.com.
  */
 
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  DataList,
+  DataListItem,
+  DataListCell,
+  DataListItemRow,
+  DataListItemCells,
+  Form,
+  FormGroup,
+  List,
+  ListItem,
+  TextInput
+} from "@patternfly/react-core";
+import Popup from './Popup';
+
 import { CONNECTION_STATE, formatIp } from "./client/network";
-
-const renderStatus = conns => {
-  return conns.length ? "Wired connected:" : "Wired not connected";
-};
-
-const renderConnections = conns => {
-  return conns.map(connection => (
-    `${connection.id} ${connection.addresses.map(formatIp).join(", ")}`
-  )).join(" - ");
-};
 
 export default function NetworkWiredStatus({ connections }) {
   const conns = connections.filter(c => c.state === CONNECTION_STATE.ACTIVATED);
+  const [editting, setEditing] = useState(null);
 
-  return <>{renderStatus(conns)} {renderConnections(conns)}</>;
+  const renderStatus = () => {
+    return conns.length ? "Wired connected:" : "Wired not connected";
+  };
+
+  const renderConnections = () => {
+    return (
+      <DataList isCompact gridBreakpoint="none" className="connections-datalist">
+        {conns.map(conn => (
+          <DataListItem key={conn.path}>
+            <DataListItemRow>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key="connection-id" isFilled={false}>
+                    {conn.id}
+                  </DataListCell>,
+                  <DataListCell key="connection-ips" isFilled={false} wrapModifier="truncate">
+                    {conn.addresses.map(formatIp).join(", ")}
+                  </DataListCell>,
+                  <DataListCell key="connection-action" isFilled={false}>
+                    <Button variant="link" isInline onClick={() => setEditing(conn)}>
+                      Modify
+                    </Button>
+                  </DataListCell>
+                ]}
+              />
+            </DataListItemRow>
+          </DataListItem>
+        ))}
+      </DataList>
+    );
+  };
+
+  return (
+    <>
+      <List isPlain>
+        <ListItem>{renderStatus(conns)}</ListItem>
+        <ListItem>{renderConnections(conns)}</ListItem>
+      </List>
+      <Popup isOpen={editting} title={`Edit "${editting?.id}" connection`}>
+        { /* FIXME: use a real onSubmit callback */}
+        <Form id="edit-connection" onSubmit={() => setEditing(null)}>
+          <FormGroup fieldId="id" label="Connectoin ID">
+            <TextInput
+              id="id"
+              name="connectionId"
+              aria-label="Connection ID"
+              value={editting?.id}
+              label="Connection ID"
+            />
+          </FormGroup>
+
+          { /* FIXME: allow editing more than one IP */ }
+          <FormGroup fieldId="ip" label="IP">
+            <TextInput
+              id="ip"
+              name="connectionIp"
+              aria-label="IP"
+              value={editting?.addresses[0]?.address}
+              label="IP"
+            />
+          </FormGroup>
+        </Form>
+
+        <Popup.Actions>
+          <Popup.Confirm form="edit-connection" type="submit" />
+          { /* FIXME: use a real onClick callback */}
+          <Popup.Cancel onClick={() => setEditing(null)} />
+        </Popup.Actions>
+      </Popup>
+    </>
+  );
 }
