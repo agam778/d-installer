d38114b1d831ffeca35245c2177132b72dfdf4e9
David Díaz González <dgonzalez@suse.de>
2022-10-14 12:36:53 +0100

    Add basic validation mechanism to IPSettingsForm

--- web/src/IpSettingsForm.jsx
+++ web/src/IpSettingsForm.jsx
@@ -20,10 +20,15 @@
  */
 
 import React, { useState } from "react";
-import { Form, FormGroup, FormSelect, FormSelectOption, TextInput } from "@patternfly/react-core";
+import { HelperText, HelperTextItem, Form, FormGroup, FormSelect, FormSelectOption, TextInput } from "@patternfly/react-core";
 import { useInstallerClient } from "./context/installer";
 import AddressesDataList from "./AddressesDataList";
 import Popup from "./Popup";
+
+const METHODS = {
+  MANUAL: "manual",
+  AUTO: "auto"
+};
 
 export default function IpSettingsForm({ connection, onClose }) {
   const client = useInstallerClient();
@@ -32,26 +37,72 @@ export default function IpSettingsForm({ connection, onClose }) {
   const [addresses, setAddresses] = useState(ipv4Settings.addresses || []);
   const [method, setMethod] = useState(ipv4Settings.method || "auto");
   const [gateway, setGateway] = useState(ipv4Settings.gateway || "");
+  const [errors, setErrors] = useState({});
+
+  const isSetAsInvalid = field => Object.keys(errors).includes(field);
+
+  const validatedAttrValue = (field) => {
+    if (isSetAsInvalid(field)) return "error";
+
+    return "default";
+  }
+  const cleanAddresses = (addrs) => addrs.filter(addr => addr.address !== "");
+
+  const cleanError = (field) => {
+    if (isSetAsInvalid(field)) {
+
+      const { [field]: _, ...nextErrors } = errors;
+      setErrors(nextErrors);
+    }
+  };
+
+  const changeName = (value) => {
+    cleanError("name");
+    setName(value);
+  };
 
   const changeMethod = (value) => {
-    let nextAddresses = addresses.filter(addr => addr.address !== "");
+    let nextAddresses = cleanAddresses(addresses);
 
     if (value === "manual" && nextAddresses.length === 0) {
       // FIXME: Use a model instead?
       nextAddresses = [{ address: "", prefix: "" }];
     }
 
+    cleanError("method");
     setAddresses(nextAddresses);
     setMethod(value);
   };
 
+  const validate = (sanitizedAddresses) => {
+    setErrors({});
+
+    const nextErrors = {};
+
+    if (name === "") {
+      nextErrors.name = "The connection must have a name";
+    }
+
+    if (method === METHODS.MANUAL && sanitizedAddresses.length === 0) {
+      nextErrors.method = "At least one address must be provided for selected mode";
+    }
+
+    setErrors(nextErrors);
+
+    return Object.keys(nextErrors).length === 0;
+  }
+
   const onSubmit = e => {
     e.preventDefault();
+
+    const sanitizedAddresses = cleanAddresses(addresses);
+
+    if (!validate(sanitizedAddresses)) return;
 
     const updatedConnection = {
       ...connection,
       id: name,
-      addresses,
+      addresses: sanitizedAddresses,
       method,
       gateway
     };
@@ -61,19 +112,31 @@ export default function IpSettingsForm({ connection, onClose }) {
     onClose();
   };
 
+  const renderError = (field) => {
+    if (!isSetAsInvalid(field)) return null;
+
+    return (
+      <HelperText>
+        <HelperTextItem variant="error">{errors[field]}</HelperTextItem>
+      </HelperText>
+    );
+  }
+
   return (
     <Popup isOpen height="medium" title={`Edit "${connection.id}" connection`}>
       {/* FIXME: use a real onSubmit callback */}
       <Form id="edit-connection" onSubmit={onSubmit}>
         <FormGroup fieldId="id" label="Name">
           <TextInput
-            id="id"
-            name="id"
+            id="name"
+            name="name"
             aria-label="Name"
             value={name}
             label="Name"
-            onChange={setName}
+            onChange={changeName}
+            validated={validatedAttrValue("name")}
           />
+          {renderError("name")}
         </FormGroup>
 
         <FormGroup fieldId="id" label="Mode">
@@ -84,10 +147,12 @@ export default function IpSettingsForm({ connection, onClose }) {
             value={method}
             label="Mode"
             onChange={changeMethod}
+            validated={validatedAttrValue("method")}
           >
             <FormSelectOption key="auto" value="auto" label="Automatic (DHCP)" />
             <FormSelectOption key="manual" value="manual" label="Manual" />
           </FormSelect>
+          {renderError("method")}
         </FormGroup>
 
         <FormGroup fieldId="gateway" label="Gateway">
